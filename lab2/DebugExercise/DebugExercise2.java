f00675bd2a25b088a431427a85f89bc9dab9e9b9
EthanGao49 <ethan.gao47@gmail.com>
2023-03-14 19:30:19 +0800

    add lab2

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -6,35 +6,44 @@ package DebugExercise;
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
-    public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+//    public static int max(int a, int b) {
+//        int w = (b - a) >> 31;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int z = ~(b - a) >> 31;
+//
+//        int max = b & w | a & z;
+//        return max;
+//    }
+    public static int max(int a, int b){
+        if(a >= b){
+            return a;
+        }else{
+            return b;
+        }
     }
 
 
     /** Returns the sum of a and b. Do not step into this function. */
-    public static int add(int a, int b) {
-        int x = a, y = b;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
+//    public static int add(int a, int b) {
+//        int x = a, y = b;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int xor, and, temp;
+//        and = x & y;
+//        xor = x ^ y;
+//
+//        while (and != 0) {
+//            and <<= 1;
+//            temp = xor ^ and;
+//            and &= xor;
+//            xor = temp;
+//        }
+//        return xor;
+//    }
+        public  static int add(int a, int b){
+            return a + b;
         }
-        return xor;
-    }
-
     /** Returns a new array where entry i is the max of
      * a[i] and b[i]. For example, if a = {1, -10, 3}
      * and b = {0, 20, 5}, this function will return {1, 20, 5}.
@@ -58,7 +67,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
